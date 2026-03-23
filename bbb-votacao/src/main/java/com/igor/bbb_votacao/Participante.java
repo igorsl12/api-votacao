e7230d6cdd19718959c6662ed0d162769c60cdda
Igor <igorsilvalima960@gmail.com>
2026-03-23 16:36:18 -0300

    Alterando urlFoto para LONGTEXT

--- bbb-votacao/src/main/java/com/igor/bbb_votacao/Participante.java
+++ bbb-votacao/src/main/java/com/igor/bbb_votacao/Participante.java
@@ -4,30 +4,27 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Column; // Importação necessária para definir o tamanho da coluna
+import jakarta.persistence.Column;
 
-@Entity // Avisa o Java que isso é uma tabela do banco
+@Entity 
 public class Participante {
     
-    @Id // Avisa que o ID é a chave primária
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // O banco gera o ID sozinho
+    @Id 
+    @GeneratedValue(strategy = GenerationType.IDENTITY) 
     private Integer id;
     
     private String nome;
 
-    // NOVO ATRIBUTO: Guarda o link da foto da internet
-    // Colocamos length = 1000 porque links de imagens podem ser bem longos
-    @Column(length = 1000) 
+    // ATUALIZADO: Usando LONGTEXT para caber o código gigante da imagem em Base64
+    @Column(columnDefinition = "LONGTEXT") 
     private String urlFoto;
 
-    // Getters e Setters (Para permitir que o Java leia e grave os dados)
     public Integer getId() { return id; }
     public void setId(Integer id) { this.id = id; }
     
     public String getNome() { return nome; }
     public void setNome(String nome) { this.nome = nome; }
 
-    // Getters e Setters da foto
     public String getUrlFoto() { return urlFoto; }
     public void setUrlFoto(String urlFoto) { this.urlFoto = urlFoto; }
 }
